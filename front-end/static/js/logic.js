3a0b4f1497ab6817270adc437f02cbffa252377b
Paula amanda Gallardo <paulaamandagallardo@Paulas-MacBook-Air.local>
2019-01-12 10:27:04 -0500

    adding tooltips

--- front-end/static/js/logic.js
+++ front-end/static/js/logic.js
@@ -38,6 +38,32 @@ d3.json(link, function(data) {
         fillOpacity: 0.4,
         weight: 1.5
       };
+    },
+    // Called on each feature
+    onEachFeature: function(feature, layer) {
+      // Set mouse events to change map styling
+      layer.on({
+        // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
+        mouseover: function(event) {
+          layer = event.target;
+          layer.setStyle({
+            fillOpacity: 0.9
+          });
+        },
+        // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
+        mouseout: function(event) {
+          layer = event.target;
+          layer.setStyle({
+            fillOpacity: 0.5
+          });
+        },
+        // When a feature (neighborhood) is clicked, it is enlarged to fit the screen
+        click: function(event) {
+          map.fitBounds(event.target.getBounds());
+        }
+      });
+      // Giving each feature a pop-up with information pertinent to it
+      layer.bindPopup("<h1>" + feature.properties.STATEFP + "</h1> <hr> <h2>" + feature.properties.CD115FP + "</h2>");
     }
   }).addTo(map);
 });
